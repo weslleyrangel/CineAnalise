b20707a72e2d8ea9c105e24af8d69a2e225e657c
Weslley Rangel <weslleyrangeldemedeirosbezerril@outlook.com.br>
2025-10-03 10:11:49 -0300

    Alterando as classes repository para utilizar o jpa

--- src/main/java/com/example/CineAnalise/repository/AnaliseRepository.java
+++ src/main/java/com/example/CineAnalise/repository/AnaliseRepository.java
@@ -1,39 +1,12 @@
 package com.example.CineAnalise.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Repository;
 
 import com.example.CineAnalise.model.Analise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 
 @Repository
-public class AnaliseRepository {
-    private final List<Analise> analises = new ArrayList<>();
-    private final AtomicLong idCounter = new AtomicLong();
-
-    public AnaliseRepository() {
-        // Dados de exemplo
-        save(new Analise() {{
-            setFilmeId(1L);
-            setNota(5);
-            setTexto("Revolucionou o cinema dos anos 90.");
-        }});
-    }
-
-    public List<Analise> findByFilmeId(Long filmeId) {
-        return analises.stream()
-                .filter(a -> a.getFilmeId().equals(filmeId))
-                .collect(Collectors.toList());
-    }
-
-    public Analise save(Analise analise) {
-        if (analise.getId() == null) {
-            analise.setId(idCounter.incrementAndGet());
-        }
-        analises.add(analise);
-        return analise;
-    }
+public interface AnaliseRepository extends JpaRepository<Analise, Long>{
 }
+   
--- src/main/java/com/example/CineAnalise/repository/FilmeRepository.java
+++ src/main/java/com/example/CineAnalise/repository/FilmeRepository.java
@@ -1,54 +1,7 @@
 package com.example.CineAnalise.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.stereotype.Repository;
-
 import com.example.CineAnalise.model.Filme;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class FilmeRepository {
-
-    private final List<Filme> filmes = new ArrayList<>();
-    private final AtomicLong idCounter = new AtomicLong();
-
-    public FilmeRepository() {
-        // Dados de exemplo
-        save(new Filme() {
-            {
-                setTitulo("Naruto");
-                setSinopse("\"Naruto\" é um anime baseado no mangá de Masashi Kishimoto, que segue a jornada de Naruto Uzumaki, um jovem ninja que sonha em se tornar Hokage, o líder mais forte de sua vila.");
-                setGenero("Shōnen, Ação, Aventura, Ninjas");
-                setAno(2002);
-            }
-        });
-        save(new Filme() {
-            {
-                setTitulo("Neo Genesis Evangelion");
-                setSinopse("Quando os Anjos começam a atacar o planeta Terra no ano de 2015, apenas um punhado de pilotos de EVA de 14 anos são capazes de detê-los. O jovem Shinji Ikari de repente se vê forçado a pilotar o EVA-01, um mecha orgânico gigante, projetado e construído pela NERV, que é a única coisa que pode parar os Anjos.");
-                setGenero("Apocalítico,Drama psicológico,Mecha");
-                setAno(1995);
-            }
-        });
-    }
-
-    public List<Filme> findAll() {
-        return filmes;
-    }
-
-    public Optional<Filme> findById(Long id) {
-        return filmes.stream().filter(f -> f.getId().equals(id)).findFirst();
-    }
-
-    public Filme save(Filme filme) {
-        if (filme.getId() == null) {
-            filme.setId(idCounter.incrementAndGet());
-        }
-        filmes.removeIf(f -> f.getId().equals(filme.getId()));
-        filmes.add(filme);
-        return filme;
-    }
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
 }
